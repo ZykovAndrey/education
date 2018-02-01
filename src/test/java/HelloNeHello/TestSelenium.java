94166fe9c0c2612bcaff780cb2c8c45c7084b435
ZykovAndrey <andruffa@yandex.ru>
2018-02-02 01:47:45 +0500

    Company

--- src/test/java/HelloNeHello/TestSelenium.java
+++ src/test/java/HelloNeHello/TestSelenium.java
@@ -8,24 +8,30 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestSelenium {
     @Test
-    public void testGoogleSearch() throws InterruptedException {
+    public void testHabraSearch() throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver", "C://Program Files (x86)//Google//WebDrivers//chromedriver.exe");
         WebDriver chromeDriver = new ChromeDriver();
-        chromeDriver.get("http://www.google.com/");
-        Thread.sleep(3000);
-        WebElement element = chromeDriver.findElement(By.name("q"));
-        Thread.sleep(3000);
-        element.sendKeys("ФБК");
-        Thread.sleep(3000);
-        element.submit();
-        Thread.sleep(3000);
-        chromeDriver.findElement(By.cssSelector("a[href*='https://fbk.info']")).click();
-        Thread.sleep(6000);
-        //WebElement testPars = chromeDriver.findElement(By.className("articleTest"));
-        //Thread.sleep(3000);
+        chromeDriver.get("https://habrahabr.ru/");
+        Thread.sleep(2000);
+        chromeDriver.findElement(By.linkText("Компании")).click();
+        Thread.sleep(2000);
+        WebElement find = chromeDriver.findElement(By.id("companies_suggest"));
+        find.sendKeys("ФБК");
+        Thread.sleep(2000);
 
-        //System.out.println(testPars.getText());
+        chromeDriver.findElement(By.id("companies"));
+        chromeDriver.findElement(By.id("company_1647"));
+        WebElement name = chromeDriver.findElement(By.cssSelector(".list-snippet__desc"));
+        chromeDriver.findElement(By.cssSelector(".table-grid__item.table-grid__item_right"));
+        chromeDriver.findElement(By.cssSelector(".stats.stats_inline"));
+        WebElement subscribers = chromeDriver.findElement(By.cssSelector(".stats__counter.stats__counter_table-grid.stats__counter_subscribers"));
+        WebElement rating = chromeDriver.findElement(By.cssSelector(".stats__counter.stats__counter_table-grid.stats__counter_rating"));
+
+        System.out.println("Название компании: " + name.getText());
+        System.out.println("Подписчики: " + subscribers.getText());
+        System.out.println("Рейтинг: " + rating.getText());
+
         chromeDriver.quit();
     }
 
